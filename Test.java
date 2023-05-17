b564b6a9cbe686a11755d3678e70d31edeac4058
Getange <emmanuelgetange48@gmail.com>
2023-05-17 13:29:49 +0300

    variables in java

---
+++ Test.java
@@ -0,0 +1,11 @@
+class Test{
+    String number="075673xxxx";
+    String name="Emmanuel";
+    public static void main(String[] args){
+        System.out.println("Java is simple!");
+
+        Test test=new Test();
+        System.out.println("My name is :" + test.name);
+        System.out.println("My number is :" + test.number);
+    }
+}
