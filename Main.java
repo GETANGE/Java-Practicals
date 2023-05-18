95b64d9825784a148983a77ff3d588af6acaa7e7
Getange <emmanuelgetange48@gmail.com>
2023-05-19 00:01:17 +0300

    last commit

--- Main.java
+++ Main.java
@@ -1,3 +1,4 @@
+//when using the public class method,name the file the same name as of the your class name.
 public class Main {
   public static void main(String[] args) {
     System.out.println("Hello world!");
--- Test.java
+++ Test.java
@@ -1,12 +1,19 @@
-class Test{
+class test{
     String number="075673xxxx";
     String name="Emmanuel";
     public static void main(String[] args){
         System.out.println("Java is simple!");
+        System.out.println("When the class is not public,you can name your class anyvalue");
 
-        Test test=new Test();
-        System.out.println("My name is :" + test.name);
-        System.out.println("My number is :" + test.number);
+        test test1=new test();//instantiation .
+        System.out.println("My name is :" + test1.name);
+        System.out.println("My number is :" + test1.number);
     }
     
+}
+
+public class Test{
+    public static void main(String[] args){
+        System.out.println("When the class is public, the name of the file has to be the class name.");
+    }
 }
