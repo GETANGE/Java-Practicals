ff21d7653d1701628dc88846d73a3ca76922a500
Getange <emmanuelgetange48@gmail.com>
2023-05-31 23:30:28 +0300

    ADD:data types in java

---
+++ HelloWorld/src/bool.java
@@ -0,0 +1,16 @@
+public class bool {
+    public static void main(String[] args){
+        boolean flag=true; //by default the boolean data type is false.
+        System.out.println(flag);//returns true.
+
+        byte range=124;//range from -128 to 127.Commonly used instead of integer to save memory.
+        System.out.println(range);
+
+        short temperature; //the default value for this primitive data type is 0.
+        temperature=-200;
+        System.out.println(temperature);
+
+        int range1=-2345;// default value for this primitive data type is 0.
+        System.out.println(range1);
+    }
+}
