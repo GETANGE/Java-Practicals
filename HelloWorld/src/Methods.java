742b68766aa7dc21d1a606582230ffb031ac8b4c
Getange <emmanuelgetange48@gmail.com>
2023-07-18 00:31:23 +0300

    ADD: added java methods

--- HelloWorld/src/Methods.java
+++ HelloWorld/src/Methods.java
@@ -9,6 +9,25 @@ public class Methods {
     static void myMethod1(String fname){
         System.out.println(fname +" "+ "Getange");
     }
+    static void myMethod2(String fname1, int age){
+        System.out.println(fname1+" is "+ age +" "+"Years");
+    }
+    static int myMethod3(int x){
+        return 5 + x;
+    }
+    static int myMethod4(int x,int y){
+        return x * y;
+    }
+    static int myMethod5(int x,int y){
+        return x / y;
+    }
+    static void myMethod6(int age){
+        if(age<18){
+            System.out.println("Not eligible for voting.....too young");
+        }else{
+            System.out.println("Eligible for voting.....");
+        }
+    }
     public static void main(String[] args) {
         Methods methods = new Methods();
         int num1 = 20, num2 = 3;
@@ -26,7 +45,14 @@ public class Methods {
         myMethod();
         myMethod1("Emmanuel");
         myMethod1("JoyElizabeth");
-        myMethod1("Mothoni");
+        myMethod1("Muthoni");
+        myMethod2("Emmanuel", 22);
+        System.out.println(myMethod3(3));
+        System.out.println(myMethod4(4,10));
+
+        int z =myMethod5(5,7);
+        System.out.println(z);
+        myMethod6(20);
     }
 
     public double getAverage(int a, int b, int c) {
