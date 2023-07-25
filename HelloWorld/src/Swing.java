32200ba0faf3f2f5e3ecad2eea543a8659548b86
Getange <emmanuelgetange48@gmail.com>
2023-07-25 13:33:19 +0300

    ADD: added Swing application in java

--- HelloWorld/src/Swing.java
+++ HelloWorld/src/Swing.java
@@ -16,6 +16,14 @@ public class Swing {
         JButton button = new JButton("Button 1");
         JButton button2 = new JButton("Button 2");
 
+        //create text fields
+        JTextField textField = new JTextField();
+        JTextField textField2 = new JTextField();
+
+        //set bounds to the text field
+        textField.setBounds(50,20 ,80, 30);
+        textField2.setBounds(50,20,80,30);
+
         button.setBounds(50,100,80,30);
         button2.setBounds(100,100,80,30);
 
@@ -23,6 +31,8 @@ public class Swing {
         button2.setBackground(Color.yellow);
         myPanel.add(button);
         myPanel.add(button2);
+        myPanel.add(textField);
+        myPanel.add(textField2);
 
         frame.setSize(400, 400);
         frame.setVisible(true);
